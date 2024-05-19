aa4df7e22c372c4e040525d9ab6891b2fac67ccd
vladislav <ilusevicv@gmail.com>
2024-05-19 20:02:20 +0300

    test cors

--- src/main/java/com/vladislav/spring/jpa/postgresql/config/CorsConfig.java
+++ src/main/java/com/vladislav/spring/jpa/postgresql/config/CorsConfig.java
@@ -1,17 +1,18 @@
 
-package com.vladislav.spring.jpa.postgresql.config;
+// package com.vladislav.spring.jpa.postgresql.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // Здесь можно указать разрешенные источники (origins)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Разрешенные HTTP методы
-                .allowedHeaders("*"); // Разрешенные заголовки
-    }
-}
+// @Configuration
+// public class CorsConfig implements WebMvcConfigurer {
+// @Override
+// public void addCorsMappings(CorsRegistry registry) {
+// registry.addMapping("/**")
+// .allowedOrigins("*") // Здесь можно указать разрешенные источники (origins)
+// .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Разрешенные
+// HTTP методы
+// .allowedHeaders("*"); // Разрешенные заголовки
+// }
+// }
